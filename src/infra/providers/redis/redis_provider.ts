ccfe3869a7e803ede0a8b6b52a0f1688bf3235a4
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-09-07 22:11:58 -0300

    feat(infra/providers): create RedisProvider

---
+++ src/infra/providers/redis/redis_provider.ts
@@ -0,0 +1,53 @@
+import { injectable } from "tsyringe"
+import Redis from "ioredis"
+import * as R from "ramda"
+import { connections } from "../../../config/redis"
+import { ApplicationError } from "../../../core/errors/application_error"
+
+@injectable()
+export class RedisProvider {
+  private redisConnections: Record<string, Redis.Redis>
+
+  private currentConnection: Redis.Redis
+
+  constructor() {
+    this.createConnections()
+  }
+
+  private createConnections = () => {
+    const redisInstances = connections.map(
+      connection =>
+        new Redis(Number(connection.port), connection.host, {
+          password: connection.password,
+        })
+    )
+
+    // eslint-disable-next-line prefer-destructuring
+    this.currentConnection = redisInstances[0]
+
+    this.redisConnections = Object.fromEntries(
+      R.zip(connections, redisInstances).map(([connection, redis]) => [
+        connection.name,
+        redis,
+      ])
+    )
+  }
+
+  connection = (connection: string) => {
+    const redisInstance = this.redisConnections[connection]
+    if (!redisInstance) {
+      throw new ApplicationError(`Unknown redis connection <${connection}>`)
+    }
+
+    this.currentConnection = redisInstance
+
+    return this
+  }
+
+  del = (...keys: Redis.KeyType[]) => this.currentConnection.del(keys)
+
+  get = (key: Redis.KeyType) => this.currentConnection.get(key)
+
+  setex = (key: Redis.KeyType, seconds: number, value: Redis.ValueType) =>
+    this.currentConnection.setex(key, seconds, value)
+}
