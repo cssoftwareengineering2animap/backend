63e33400775c6997c2259812cd1ea16bdba3ffcd
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-10-04 22:58:35 -0300

    this is annoying

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,11 @@
 import "reflect-metadata"
+import fs from "fs"
 import * as container from "./src/presentation/rest/container"
+
+const PUBLIC_FOLDER = "./public"
+
+if (!fs.existsSync(PUBLIC_FOLDER)) {
+  fs.mkdirSync(PUBLIC_FOLDER)
+}
 
 container.register()
